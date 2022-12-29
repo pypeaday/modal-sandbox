b5f19ee41545c0b34ba5688189febdb74f10f85a
pypeaday <pypeaday@pype.dev>
2022-12-28 19:48:15 -0600

    add say_hi_on_cron which is an example of a Modal cron job deployment

---
+++ modal_sandbox/say_hi_on_cron.py
@@ -0,0 +1,15 @@
+import modal
+import os
+import time
+
+stub = modal.Stub()
+
+
+@stub.function(schedule=modal.Period(minutes=1))
+def say_hi():
+    now = time.ctime()
+    print(f"Hello {os.environ.get('USER', 'Rodney')} at {now}")
+
+
+if __name__ == "__main__":
+    say_hi()
