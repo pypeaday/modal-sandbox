00fb6900aa9a206368f52194882f65fc5755a141
pypeaday <pypeaday@pype.dev>
2022-12-28 20:58:19 -0600

    add app to print info about pandas package

--- modal_sandbox/say_hi_on_cron.py
+++ modal_sandbox/say_hi_on_cron.py
@@ -4,6 +4,43 @@ import time
 
 stub = modal.Stub()
 
+my_image = modal.Image.debian_slim().pip_install(["pandas", "requests"])
+
+
+if stub.is_inside():
+    import pandas as pd
+    import requests
+
+
+def get_url(package: str) -> str:
+    return f"https://pypi.org/pypi/{package}/json"
+
+
+def read_api_data(url, package: str):
+    print(f"Pandas version in container: {pd.__version__}")
+    # Make a request to the API
+    response = requests.get(url)
+
+    # Check for successful response
+    if response.status_code == 200:
+        # Convert the response to JSON format
+        data = response.json()
+
+        print(f"Author of {package} is {data['info']['author']}")
+    else:
+        print(f"Got back {response.status_code}")
+
+
+@stub.function(
+    image=my_image,
+    schedule=modal.Period(minutes=1),
+)
+def print_info():
+    package = "pandas"
+    # return PyPi API URL
+    url = get_url(package)
+    read_api_data(url, package)
+
 
 @stub.function(
     schedule=modal.Period(minutes=1), secret=modal.Secret.from_name("my-dummy-secret")
@@ -17,3 +54,5 @@ def say_hi():
 
 if __name__ == "__main__":
     stub.deploy("say_hi")
+
+    stub.deploy("print_info")
