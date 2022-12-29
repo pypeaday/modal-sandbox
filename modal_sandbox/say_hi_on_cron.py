b0c03e55a01960cd5f6f47e8fcdeff98c58ad128
pypeaday <pypeaday@pype.dev>
2022-12-28 19:53:29 -0600

    add a dummy secret to cron

--- modal_sandbox/say_hi_on_cron.py
+++ modal_sandbox/say_hi_on_cron.py
@@ -5,10 +5,14 @@ import time
 stub = modal.Stub()
 
 
-@stub.function(schedule=modal.Period(minutes=1))
+@stub.function(
+    schedule=modal.Period(minutes=1), secret=modal.Secret.from_name("my-dummy-secret")
+)
 def say_hi():
     now = time.ctime()
+    secret = os.environ.get("dummy-secret")
     print(f"Hello {os.environ.get('USER', 'Rodney')} at {now}")
+    print(f"{secret=}")
 
 
 if __name__ == "__main__":
